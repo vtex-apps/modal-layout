271aa078f767bff5be3e12a1419b87829232d215
klynger <rafael.klynger@gmail.com>
2020-05-15 15:53:51 -0300

    Add findCSSHandles helper function

---
+++ react/__mocks__/testUtils.ts
@@ -0,0 +1,13 @@
+export function findCSSHandles(
+  container: HTMLElement,
+  handles: readonly string[]
+) {
+  const foundNodes = handles
+    .map(handle => {
+      const foundNodesInner = container.getElementsByClassName(handle)
+      return foundNodesInner.length > 0 ? handle : ''
+    })
+    .filter(result => result !== '')
+
+  return foundNodes
+}
